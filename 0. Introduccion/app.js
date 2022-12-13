a0f57b31ba07a04c693e300e4859628bd28e2fd7
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-12-12 20:41:06 -0600

    0. Inicializa repositorio y agrega ejemplo de introducción al curso

---
+++ 0. Introduccion/app.js
@@ -0,0 +1,7 @@
+console.log(document);
+document.querySelector('h1');
+console.log(document.querySelector('h1'));
+document.querySelector('h1').textContent = 'Texto desde JS';
+
+console.log(document.querySelector('.parrafo'));
+document.querySelector('.parrafo').textContent = 'Párrafo dinámico desde JS.'
