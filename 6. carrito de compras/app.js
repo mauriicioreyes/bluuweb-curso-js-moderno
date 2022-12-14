439071cb1eb8300d2e9bf9027ddfb0c61b0c608c
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-12-13 22:46:02 -0600

    6. Crea proyecto sobre carrito de compras
    Implementa la estructura principal para cards en HTML
    Mediante JS accede a los datos del archivo JSON
    Coloca cada elemento del JSON en un card de HTML

---
+++ 6. carrito de compras/app.js
@@ -0,0 +1,71 @@
+const cards = document.getElementById('cards');
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+// console.log(templateCard);
+
+// Colección de objetos
+let carrito = {};
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+});
+
+cards.addEventListener('click', e => {
+    addCarrito(e);
+});
+
+// Leer elementos del JSON
+const fetchData = async () => {
+    try {
+        const res = await fetch('api.json');
+        const data = await res.json();
+        // console.log(data);
+        pintarCards(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const pintarCards = data => {
+    console.log(data);
+    data.forEach(producto => {
+        // console.log(producto);
+        templateCard.querySelector('h5').textContent = producto.title;
+        templateCard.querySelector('p').textContent = producto.precio;
+        templateCard.querySelector('img').setAttribute('src', producto.thumbnailUrl);
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id;
+        
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    });
+    cards.appendChild(fragment);
+};
+
+const addCarrito = e => {
+    // console.log(e.target);
+    if (e.target.classList.contains('btn-dark')) {
+        // console.log(e.target.parentElement);
+        setCarrito(e.target.parentElement);
+
+    }
+    e.stopPropagation();
+};
+
+const setCarrito = objeto => {
+    // console.log(objeto);
+    const producto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        title: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad: 1
+    }
+
+    if (carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+
+    carrito[producto.id] = {...producto};
+    console.log(producto);
+    console.log(carrito);
+};
